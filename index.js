9f16064cd970bbe0e2c04cc3dba9d7f66d25abe1
Sara Alkhamri <s.alkhamri@gmail.com>
2020-04-29 17:52:58 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const minimist = require('minimist')
+
 module.exports = () => {
-    console.log('Welcome to the outside!')
-  }
+
+  //The reason we remove the first two arguments with .slice(2) is because the first arg will always be the interpreter followed by
+  // the name of the file being interpreted. We only care about the arguments after that.  
+  const args = minimist(process.argv.slice(2))
+  console.log(args)
+}
