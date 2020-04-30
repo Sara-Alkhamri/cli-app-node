c7f98289a2ea57c4eb1610f952e18b39d909559b
Sara Alkhamri <s.alkhamri@gmail.com>
2020-04-29 18:48:26 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const minimist = require('minimist')
+const minimist = require('minimist')
 
 module.exports = () => {
-
-  //The reason we remove the first two arguments with .slice(2) is because the first arg will always be the interpreter followed by
-  // the name of the file being interpreted. We only care about the arguments after that.  
   const args = minimist(process.argv.slice(2))
-  console.log(args)
+  const cmd = args._[0]
+
+  switch (cmd) {
+    case 'today':
+      require('./cmds/today')(args)
+      break
+    default:
+      console.error(`"${cmd}" is not a valid command!`)
+      break
+  }
 }
