13ea1514c1d2096788aac1eece17328d115132c7
Sara Alkhamri <s.alkhamri@gmail.com>
2020-04-29 19:18:40 -0700

    Added forecast case to switch statement in index.js

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ module.exports = () => {
       require('./cmds/today')(args)
       break
 
+    case 'forecast':
+      require('./cmds/forecast')(args)
+      break  
+
+      
     case 'version':
       require('./cmds/version')(args)
       break
